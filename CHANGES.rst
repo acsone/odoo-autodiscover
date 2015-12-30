e9c195c7169cf4b76c1321221e2b01eeb160fa1f
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-12-30 15:53:47 +0100

    [FIX] better odoo.py discovery
    odoo-autodiscover.py: more reliable way to discover and import
    the official odoo.py script, so it will now work when Odoo is installed
    from the deb package

--- CHANGES.rst
+++ CHANGES.rst
@@ -3,7 +3,9 @@ Changes
 
 1.0.1 (unreleased)
 ------------------
-- 
+- [FIX] odoo-autodiscover.py: more reliable way to discover and import
+  the official odoo.py script, so it will now work when Odoo is installed
+  from the deb package
 
 1.0.0 (2015-12-28)
 ------------------
--- odoo-autodiscover.py
+++ odoo-autodiscover.py
@@ -3,14 +3,20 @@
 # © 2015 ACSONE SA/NV
 # License LGPLv3 (http://www.gnu.org/licenses/lgpl-3.0-standalone.html)
 
-# this imports the odoo.py script (not completely clean, but there is no other
-# way due to the way the upstream script is packaged)
-import odoo
+import imp
+import sys
+from distutils.spawn import find_executable
 
 from odoo_autodiscover import monkey
 
 
 def main():
+    odoo_py = find_executable('odoo.py')
+    if not odoo_py:
+        sys.stderr.write('The odoo.py executable could not be found, '
+                         'therefore odoo-autodiscover.py can not start.')
+        sys.exit(1)
+    odoo = imp.load_source('odoo', odoo_py)
     monkey.patch()
     odoo.main()
 
